901b3320f93eaa54074d6695940af94ed0d30e22
kidnixt <kidnixt@gmail.com>
2024-02-06 20:36:41 -0300

    bert wrapper finished

--- model_wrapper/bert_small_wrapper.py
+++ model_wrapper/bert_small_wrapper.py
@@ -17,6 +17,7 @@ class BertSmallWrapper(Wrapper):
             output = self.model(tokens.input_ids)
             logits = output.logits
             probs = torch.softmax(logits, dim=-1)
+        print(probs.shape)
 
         word_probabilities = {}
         for word in symbols:
@@ -27,14 +28,29 @@ class BertSmallWrapper(Wrapper):
             word_tokens = self.tokenize(word)
             print(word_tokens)
             print(word_tokens.input_ids)
-            word_probs = probs[:, word_tokens.input_ids[0]]
+            #transform tensor to list
+            word_tokens.input_ids = word_tokens.input_ids.tolist()
+            print(word_tokens.input_ids)
+
+            #remove the first and last token
+            word_tokens.input_ids = [i[1:-1] for i in word_tokens.input_ids]
+            print(word_tokens.input_ids)
+            print(word_tokens.input_ids[-1])
+            #get the probability of the word
+            word_probs = probs[0, -1, word_tokens.input_ids[-1]]
+
             total_word_probs = sum(word_probs)
             total_word_probs /= len(word_probs)
             word_probabilities[word] = total_word_probs.item()
 
+
+        #normalize
         total = sum(word_probabilities.values())
         for word in word_probabilities:
             word_probabilities[word] /= total
+
+
+        print(word_probabilities)
 
         return word_probabilities
     
